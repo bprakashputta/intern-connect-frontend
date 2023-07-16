a6a105531ba5dbeaa93e1139df2e20fc9f45ed0f
Putta Bhanu Prakash <bhanu.putta1999@gmail.com>
2023-07-16 13:24:47 +0530

    Updated changes for build and SingleJob issues

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ import { useContext } from "react";
 import JobList from "./components/Jobs/JobList";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { theme } from "./Component/theme";
-import SingleJob from "./components/Jobs/singleJob";
+import SingleJob from "./components/Jobs/SingleJob";
 import UserList from "./components/Users/UserList";
 import AddJob from "./components/Jobs/AddJob";
 import Profile from "./components/Employer/Profile";
